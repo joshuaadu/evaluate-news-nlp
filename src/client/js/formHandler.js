1cbd0be6159bdf5a8037eac9577297d812080de0
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-09-03 13:27:47 +0000

    feat: Require a url input and give error message

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,16 +2,23 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
+    document.querySelector('.table-body').style.display = 'none'
     let formText = document.getElementById('text').value
-    const data = {text: formText}
-    Client.meaningCloudRequest(data)
+    if(Client.checkForName(formText)) {
+        const data = {text: formText}
+        Client.meaningCloudRequest(data)
+        console.log("::: Form Submitted :::")
 
-    console.log("::: Form Submitted :::")
-    // fetch('http://localhost:8080/test')
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     document.getElementById('results').innerHTML = res.message
-    // })
+    }else {
+        alert('Please enter a valid url!')
+    }
+    
+
 }
+
+
+
+
+
 
 export { handleSubmit }
