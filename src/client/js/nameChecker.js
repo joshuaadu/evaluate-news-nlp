73fdd3d91461e4807f428957cd35aed4c3257397
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-09-03 13:14:05 +0000

    feat: Check validity of user input

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,16 +1,6 @@
-function checkForName(inputText) {
-    console.log("::: Running checkForName :::", inputText);
-    let names = [
-        "Picard",
-        "Janeway",
-        "Kirk",
-        "Archer",
-        "Georgiou"
-    ]
-
-    if(names.includes(inputText)) {
-        alert("Welcome, Captain!")
-    }
+function checkForName(url) {
+    const testUrl = new RegExp(/^(http|https):\/\/[^ "]+$/)
+    return testUrl.test(urls)
 }
 
 export { checkForName }
